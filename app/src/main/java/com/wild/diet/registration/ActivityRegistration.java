e58db06f47683b3c98246668a3de8eb15e8dc3da
Nikolay <ny@wsoteam.com>
2019-02-21 00:53:46 +0300

    init

---
+++ app/src/main/java/com/wild/diet/registration/ActivityRegistration.java
@@ -0,0 +1,15 @@
+package com.wild.diet.registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wild.diet.R;
+
+public class ActivityRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
