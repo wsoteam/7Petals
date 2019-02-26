80246885f674ee4f2ab1458f0f8e42d7a54db3a8
wkazak <wk@wsoteam.com>
2019-02-26 17:43:17 +0300

    add
    - view holder and adapter (multi)

---
+++ app/src/main/java/com/wsoteam/diet/MainScreen/EatingAdapter.java
@@ -0,0 +1,38 @@
+package com.wsoteam.diet.MainScreen;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.wsoteam.diet.MainScreen.POJOEating.Eating;
+
+import java.util.List;
+
+public class EatingAdapter extends RecyclerView.Adapter<EatingViewHolder> {
+    List<Eating> listOfEating;
+    Context context;
+
+    public EatingAdapter(List<Eating> listOfEating, Context context) {
+        this.listOfEating = listOfEating;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public EatingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        return new EatingViewHolder(layoutInflater, parent);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EatingViewHolder holder, int position) {
+        holder.bind(listOfEating.get(position), context);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listOfEating.size();
+    }
+}
---
+++ app/src/main/java/com/wsoteam/diet/MainScreen/EatingViewHolder.java
@@ -0,0 +1,30 @@
+package com.wsoteam.diet.MainScreen;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.wsoteam.diet.MainScreen.POJOEating.Eating;
+import com.wsoteam.diet.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class EatingViewHolder extends RecyclerView.ViewHolder {
+    @BindView(R.id.ivProductImage) ImageView ivProductImage;
+    @BindView(R.id.tvNameOfFood) TextView tvNameOfFood;
+
+    public EatingViewHolder(LayoutInflater layoutInflater, ViewGroup parent) {
+        super(layoutInflater.inflate(R.layout.ms_item_eating_list, parent, false));
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void bind(Eating eating, Context context){
+        Glide.with(context).load(eating.getUrlOfImages()).into(ivProductImage);
+        tvNameOfFood.setText(eating.getName());
+    }
+}
