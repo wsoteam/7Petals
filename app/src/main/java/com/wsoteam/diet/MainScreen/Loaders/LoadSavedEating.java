31f5c3789c1c3137e2c80aee143c281c78b33a52
wkazak <wk@wsoteam.com>
2019-02-27 09:31:16 +0300

    add
    - async load data from local db
    - post update ui

--- app/src/main/java/com/wsoteam/diet/MainScreen/Loaders/LoadSavedEating.java
+++ app/src/main/java/com/wsoteam/diet/MainScreen/Loaders/LoadSavedEating.java
@@ -1,16 +1,18 @@
 package com.wsoteam.diet.MainScreen.Loaders;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.wsoteam.diet.MainScreen.POJOEating.Breakfast;
 import com.wsoteam.diet.MainScreen.POJOEating.Dinner;
+import com.wsoteam.diet.MainScreen.POJOEating.Eating;
 import com.wsoteam.diet.MainScreen.POJOEating.Lunch;
 import com.wsoteam.diet.MainScreen.POJOEating.Snack;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class LoadSavedEating extends AsyncTask<Void, Void, List> {
+public class LoadSavedEating extends AsyncTask<Void, Void, List<List<Eating>>> {
     @Override
     protected List doInBackground(Void... voids) {
 
@@ -24,6 +26,11 @@ public class LoadSavedEating extends AsyncTask<Void, Void, List> {
         list.add(lunches);
         list.add(dinners);
         list.add(snacks);
+
+        Log.e("LOL", String.valueOf(breakfasts.size()));
+        Log.e("LOL", String.valueOf(lunches.size()));
+        Log.e("LOL", String.valueOf(dinners.size()));
+        Log.e("LOL", String.valueOf(snacks.size()));
 
         return list;
     }
--- app/src/main/java/com/wsoteam/diet/MainScreen/MainActivity.java
+++ app/src/main/java/com/wsoteam/diet/MainScreen/MainActivity.java
@@ -8,11 +8,22 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 
 import com.wsoteam.diet.BranchFoodSearch.Activities.ActivitySearch;
+import com.wsoteam.diet.MainScreen.Loaders.LoadSavedEating;
+import com.wsoteam.diet.MainScreen.POJOEating.Breakfast;
+import com.wsoteam.diet.MainScreen.POJOEating.Dinner;
+import com.wsoteam.diet.MainScreen.POJOEating.Eating;
+import com.wsoteam.diet.MainScreen.POJOEating.Lunch;
+import com.wsoteam.diet.MainScreen.POJOEating.Snack;
 import com.wsoteam.diet.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +36,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @BindView(R.id.rvListOfSnacks) RecyclerView rvListOfSnacks;
     @BindView(R.id.nav_view_g) NavigationView navViewG;
     @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    private EatingAdapter breakfastAdapter, lunchAdapter, dinnerAdapter, snackAdapter;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateUI();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +55,44 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navViewG.setNavigationItemSelectedListener(this);
+        //testFill();
+        rvListOfBreakfast.setLayoutManager(new LinearLayoutManager(this));
+        rvListOfLunch.setLayoutManager(new LinearLayoutManager(this));
+        rvListOfDinner.setLayoutManager(new LinearLayoutManager(this));
+        rvListOfSnacks.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void testFill() {
+        for (int i = 0; i < 10; i++) {
+            new Breakfast("name", "url", 100, 100, 100,
+                    100, 100, 1, 1, 2019).save();
+            new Lunch("name", "url", 100, 100, 100,
+                    100, 100, 1, 1, 2019).save();
+            new Dinner("name", "url", 100, 100, 100,
+                    100, 100, 1, 1, 2019).save();
+            new Snack("name", "url", 100, 100, 100,
+                    100, 100, 1, 1, 2019).save();
+        }
+    }
+
+    private void updateUI() {
+        try {
+            List<List<Eating>> eatingList = new LoadSavedEating().execute().get();
+
+            breakfastAdapter = new EatingAdapter(eatingList.get(0), this);
+            lunchAdapter = new EatingAdapter(eatingList.get(1), this);
+            dinnerAdapter = new EatingAdapter(eatingList.get(2), this);
+            snackAdapter = new EatingAdapter(eatingList.get(3), this);
+
+            rvListOfBreakfast.setAdapter(breakfastAdapter);
+            rvListOfLunch.setAdapter(lunchAdapter);
+            rvListOfDinner.setAdapter(dinnerAdapter);
+            rvListOfSnacks.setAdapter(snackAdapter);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
     @OnClick(R.id.fabAddFood)
